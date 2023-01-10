4bf47937e4063ac1a0d7f9d63f9ecdb79d0c89d0
Anthony <tonyudeagbala@gmail.com>
2023-01-10 20:26:23 +0100

    chore: create user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,70 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .manager import CustomUserManager
+from django.dispatch import receiver
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.core.mail import send_mail 
+from django.conf import settings
 
-# Create your models here.
+
+class TrackingModel(models.Model):  
+    """
+        Most of the models have have these two fields below as recurrent fields, 
+        so to maintain a dry code it is wise to separate them this way.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+
+class User(AbstractBaseUser, TrackingModel, PermissionsMixin):
+    username = models.CharField(verbose_name=_("Username"), max_length=255, unique=True)
+    first_name = models.CharField(verbose_name=_("First Name"), max_length=50)
+    last_name = models.CharField(verbose_name=_("Last Name"), max_length=50)
+    email = models.EmailField(verbose_name=_("Email Address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.username
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+
+    email_plaintext_message = "{}/{}?token={}".format(settings.DOMAIN, reverse('password_reset:reset-password-request'), reset_password_token.key)
+
+    send_mail(
+        # title:
+        "Password Reset",
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@info.cella.com",
+        # to:
+        [reset_password_token.user.email]
+    )
+    return {"token": reset_password_token.key}
