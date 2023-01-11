6b9b17b3cb6774dbe85026b87e4aecb591c3a23c
Anthony <tonyudeagbala@gmail.com>
2023-01-11 15:21:14 +0100

    chore: create models

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from account.models import TrackingModel, User
+from jsonfield import JSONField
 
-# Create your models here.
+
+class Website(TrackingModel, models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(default="name", max_length=50, null=False)
+    url = models.URLField(null=False)
+    state = models.BooleanField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserNotification(TrackingModel, models.Model):
+    web = models.ForeignKey(Website, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+
+class WebLog(TrackingModel, models.Model):
+    web = models.ForeignKey(Website, on_delete=models.CASCADE)
+    web_log = models.JSONField(null=True)
+    
+    def __str__(self):
+        return self.web.name
