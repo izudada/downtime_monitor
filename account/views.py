c82dc73e2a42a4672f5d00b7ca83ffeff3fcbdba
Anthony <tonyudeagbala@gmail.com>
2023-01-11 17:06:29 +0100

    fix: add comments

--- account/views.py
+++ account/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.authentication import TokenAuthentication
 from .serializers import RegisterSerializer
 
 from django.contrib.auth import login, logout
