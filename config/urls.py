975037805213ec608dfcdce08a5508263da85504
Anthony <tonyudeagbala@gmail.com>
2023-01-11 13:50:57 +0100

    chore: add monitor url

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls'))
+    #   urls for account app
+    path('account/', include('account.urls')),
+    #   urls for monitor app
+    path('monitor/', include('monitor.urls'))
 ]
 string_var = "Web Monitor (Admin Portal)"
 admin.site.site_header = f"{string_var}"
