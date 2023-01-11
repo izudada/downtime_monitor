ab5229eca2cb1b0fcb8f785169b5961cdae2fa7e
Anthony <tonyudeagbala@gmail.com>
2023-01-11 12:41:11 +0100

    feat: logout

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('auth/register/', views.register, name="register"),
     path('auth/login/', views.login_user, name="login"),
+    path('auth/logout/', views.user_logout, name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication
 from .serializers import RegisterSerializer
 
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import check_password
 from rest_framework.authtoken.models import Token
@@ -55,7 +55,6 @@ def register(request):
         return Response(status=status.HTTP_400_BAD_REQUEST, data=message)
 
 
-#   Login API
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def login_user(request):
@@ -89,3 +88,16 @@ def login_user(request):
 
     else:
         raise ValidationError({"400": f'Account doesnt exist'})
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def user_logout(request):
+    """
+        API to logout a user
+    """
+    request.user.auth_token.delete()
+
+    logout(request)
+
+    return Response({'message': 'User logged out successfully'})
