c03c785e7e4b2468ef9c57b5f5b901bc27e36f33
Anthony <tonyudeagbala@gmail.com>
2023-01-11 17:03:14 +0100

    fix: edit models

--- monitor/models.py
+++ monitor/models.py
@@ -6,16 +6,12 @@ from jsonfield import JSONField
 class Website(TrackingModel, models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(default="name", max_length=50, null=False)
+    emails = models.JSONField(null=True)
     url = models.URLField(null=False)
     state = models.BooleanField(null=True)
 
     def __str__(self):
         return self.name
-
-
-class UserNotification(TrackingModel, models.Model):
-    web = models.ForeignKey(Website, on_delete=models.CASCADE)
-    email = models.EmailField()
 
 
 class WebLog(TrackingModel, models.Model):
