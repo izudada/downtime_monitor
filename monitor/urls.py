4d169f647317e245d32d8ee64bfc979f87d2cebe
Anthony <tonyudeagbala@gmail.com>
2023-01-12 01:47:44 +0100

    chore: add logs url

--- monitor/urls.py
+++ monitor/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    
+    path('websites/', views.WebsiteAPIView.as_view(), name="create_website"),
+    path('websites/<int:id>/logs/', views.get_logs, name="view_logs")
 ]
